d5f8f6d1763d65b2e73aad1f88edc8ef1222767b
Dumitru Gotca <dum.gotca@gmail.com>
2023-11-25 19:40:03 +0200

    add holiday page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.sarbatori365.info',
+  siteUrl: process.env.SITE_URL || 'https://www.sarbatori.net/',
   generateRobotsTxt: true,
 }
