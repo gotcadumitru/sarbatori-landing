9b149c9d3c89f58ee1535b8548b3a7f7b945be1d
Dumitru Gotca <dum.gotca@gmail.com>
2023-11-25 17:13:15 +0200

    fix header animation

--- shared/ui/CircleShape/CircleShape.tsx
+++ shared/ui/CircleShape/CircleShape.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import classNames from 'classnames'
 import type { FC, PropsWithChildren } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import { useEffect, useState } from 'react'
 import classes from './circleShape.module.css'
 import './circleShapeAnim.css'
 
@@ -21,15 +23,20 @@ export const CircleShape: FC<PropsWithChildren<CircleShapeProps>> = ({
   className,
   position,
   isDisplayed,
-}) => (
-  <CSSTransition in={isDisplayed} timeout={500} classNames='circle-anim'>
+}) => {
+  const [isCircleShapeDisplayed, setIsCircleShapeDisplayed] = useState(false)
+  useEffect(() => {
+    setIsCircleShapeDisplayed(!!isDisplayed)
+  }, [isDisplayed])
+  return (
     <div
       className={classNames(classes.circleShape, className, {
+        [classes.isDisplayed]: isCircleShapeDisplayed,
         [classes.circleShapeTopRight]: position === CircleShapePosition.TOP_RIGHT,
         [classes.circleShapeTopLeft]: position === CircleShapePosition.TOP_LEFT,
         [classes.circleShapeBottomRight]: position === CircleShapePosition.BOTTOM_RIGHT,
         [classes.circleShapeBottomLeft]: position === CircleShapePosition.BOTTOM_LEFT,
       })}
     />
-  </CSSTransition>
-)
+  )
+}
