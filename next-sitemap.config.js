b37fb16c681e6987f6d28fb5fbe54d56035eb870
Dumitru Gotca <dum.gotca@gmail.com>
2023-12-30 16:22:44 +0200

    edit sitemap settings

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.sarbatori.net/',
+  siteUrl: process.env.SITE_URL || 'https://www.sarbatori.net',
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public',
+  alternateRefs: [
+    {
+      href: `${process.env.SITE_URL}/en`,
+      hreflang: 'en',
+    },
+    {
+      href: `${process.env.SITE_URL}/ro`,
+      hreflang: 'ro',
+    },
+    {
+      href: `${process.env.SITE_URL}/ru`,
+      hreflang: 'ru',
+    },
+  ],
 }
