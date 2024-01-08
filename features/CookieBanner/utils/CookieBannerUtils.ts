7e9219baff6bbc18cef948ae91bc4a8d97416f0b
Dumitru Gotca <dum.gotca@gmail.com>
2024-01-08 13:08:02 +0200

    fix error

--- features/CookieBanner/utils/CookieBannerUtils.ts
+++ features/CookieBanner/utils/CookieBannerUtils.ts
@@ -1,11 +1,9 @@
 'use client'
 
 export const getLocalStorage = (key: string, defaultValue: any) => {
-  const stickyValue = localStorage.getItem(key)
+  const stickyValue = typeof localStorage !== 'undefined' && localStorage.getItem(key)
 
-  return stickyValue !== null && stickyValue !== 'undefined'
-    ? JSON.parse(stickyValue)
-    : defaultValue
+  return stickyValue && stickyValue !== 'undefined' ? JSON.parse(stickyValue) : defaultValue
 }
 
 export const setLocalStorage = (key: string, value: any) => {
