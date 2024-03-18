b3108376ae328279c716510313d57a5a75673375
Dumitru Gotca <dum.gotca@gmail.com>
2024-03-19 00:35:07 +0200

    bullet

--- holidayImages/images.js
+++ holidayImages/images.js
@@ -12,123 +12,121 @@ const generateSomething = async () => {
       Authorization: `Bearer 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`,
     },
     body: JSON.stringify({
-      model: 'gpt-4',
+      model: 'gpt-4-0125-preview',
       messages: [
         {
           role: 'user',
-          content: `
-As we get started, here are some general instructions for you to follow.
-Custom Instructions for Forms
+          content: `Custom Instructions
 Here are some instructions for you:
 - You are an expert on all subject matters
 - Provide accurate and factual answers
-- Provide detailed results that clearly describe practical considerations of what you generate
+- Offer both pros and cons when discussing solutions or opinions
+- Provide detailed explanations
 - Be highly organized and provide mark up visually 
-- When you see handlebars (like this {{the Role}}) in the form, insert the value of "the Role". For example, if you know that the Role is Accounting Manager and you see Job Description for {{the Role}}, when you generate, give the output Job Description for Accounting Manager.
 - No need to disclose you are an AI, e.g., do not answer with "As a large language model..." or "As an artificial intelligence..."
 - Don't mention your knowledge cutoff
-- Be excellent at reasoning and business strategy
-- When reasoning, perform a step-by-step thinking before you answer the question or generate
+- Be excellent at reasoning
+- When reasoning, perform a step-by-step thinking before you answer the question
 - If you speculate or predict something, inform me
 - If you cite sources, ensure they exist and include URLs at the end
 - Maintain neutrality in sensitive topics
-- Focus strongly on proven business ideas, concepts, and strategies
+- Focus strongly on out-of-the-box, unique, and creative ideas
 - Only discuss safety when it's vital and not clear
+- Summarize key takeaways at the end of detailed explanations
 - If the quality of your response has decreased significantly due to my custom instructions, please explain the issue
+- Write short sentences.
 - Avoid multiple thoughts in one sentence.
 - Use 1–2 breakpoints to space out paragraphs.
 - Avoid 3+ sentence paragraphs.
 - Provide analogies/metaphors to simplify ideas, concepts, and complex topics
-- When filling out a form or template, follow the instructions exactly as you're asked to do in the form or template.
-- Avoid the use of flowery language
-- In what you generate, don't abbreviate words (e.g. don't shorten "collaborate" to "collab."
-Now you're going to find two example job descriptions (hereafter, "the Example Descriptions").
-Now, in between the <jd1> and <jd2> tags respectively, you'll find the text from two job descriptions indicative of the kind of jobs I'm targeting (hereafter collectively "My Job Descriptions" and individually "My Job Description"). 
+- When creating characters always consider demographic diversity in terms of race, ethnicity, sexual orientation, gender expression, etc..
+- Avoid flowery language (e.g. "flourished", "bountiful", "plentiful", "pioneered", "thrilled", "I hope this email finds you well").
+- Generate all sections of any forms completely. For example, never generate "[Continue for all ...]"
+Now, I'm developing content about a work role (hereafter, "the Role") that I've including in a resume I'm writing (hereafter, "the Resume").
 
-<jd1>
-Job Description
-We are a small, dynamic product and services company based in Bristol, UK. We have a focus on the defence and security sectors and actively seek and enjoy delivering innovative and engaging outcomes. We have the in house skills to design and build mechanical and software products and when required integrate these into customer platforms and systems.
+In between the <info> tags, you'll find some general information about the Role (hereafter, "the Information").
+<info>
+• Organization Name: Assist Software
+• Industry: Software development
+• Total # Employees: ~350
+• Annual Revenue (if known): ~$20 million
+• Recognizable Partners: ZeroBounce,English Attack!,Seaplify
+• Job Title: Front-End Developer
+• # of People on Your Team: 3-12
+• Here's a general description of my work here (hereafter, "the Description"): 
+</info>
 
-Why we're hiring:
-We are looking for a Mid level Front End Developer to produce innovative and scalable software solutions within our small development team.
-You’ll be part of a cross-functional team that’s responsible for the design of new products with your focus on the development life cycle, from conception to deployment.
-You should be comfortable around JavaScript,  HTML, CSS/SCSS, Typescript and React. You should also be a team player with an intuition for good design and utility.
-Successful candidates will have a minimum of three years’ recent experience.
-Work with our engineering teams and product managers to ideate software solutions
-Design, build and maintain efficient, useable, and reliable front-end applications
-Develop product features with minimal supervision
-Turn wireframes into interactive prototypes
-Turn prototypes into production ready code
-Build the front-end of applications through appealing visual design
-Test software to ensure responsiveness and efficiency
-Troubleshoot, debug and upgrade software
-Build features and applications with a mobile responsive design
-Work with Engineering and Capability Delivery to improve software
+Now, in between the <accomplishments> tags, you'll find some of my key accomplishments (hereafter, "the Accomplishments") in this Role.
 
-The Person:
-Self-motivated with the ability to work independently with minimal supervision
-Take pride in producing and maintaining work to a high standard
-Be a creative thinker able to recognise and solve problems by thinking outside the box
-Willingness to adopt new techniques and technologies and stay up to date with industry best practices
-Understanding of Agile methodologies
-Minimum of three years’ recent experience with all of the skills below.
+<accomplishments>
+I've been involved in several projects for multiple clients from different countries. One of the projects I worked on was for a call center company, where my team and I significantly improved call quality using technologies like WebRTC and WebSocket. I independently developed the web part of a platform that analyzes calls made within a company, providing a transcript and a brief AI-generated call summary at the end, which greatly assists managers in understanding agent efficiency. Additionally, I contributed to developing an SMS platform using React-Native and WebSocket, allowing call centers to utilize another communication channel, resulting in increased clientele and company revenue.
 
-What skills and experience will you need?
-Proven experience as a Front End Developer
-Experience of being part of a development team
-Experience of developing desktop and mobile applications
-Excellent HTML/CSS skill
-Strong JavaScript and Typescript knowledge
-Experience with React
-Experience of design tools such as Figma and Adobe XD
-Experience of container orchestration (we use Docker) would be beneficial
-Experience of Linux would be an advantage
-Strong sense of web design and an understanding of UX best practices, cross-platform compatibility, responsive design principles
-Exposure to test driven development and integration testing would be beneficial
-Experience working with Git an advantage
-Experience working with development team tools (we use Azure DevOps)
-Eagerness to work in a collaborative environment, including teaching and learning from teammates
-Excellent communication and teamwork skills
-Great attention to detail
-An analytical, inquisitive mind
+During my time at Assist, I often took on the role of mentoring beginners in technologies like React, Redux, Next.js, and React-Native for periods ranging from 1 to 6 months. I also served as the primary person helping newcomers to our projects. In addition to mentoring, I taught a React-Native course for internal company employees who were primarily web-focused and wanted to learn mobile development. This addressed the need for mobile applications while the number of employees proficient in cross-platform technologies for Android and iOS was limited.
 
-Culture:
-Flexible, friendly and fun, but driven to making the business a success
-Tech and process driven – we make tech, we love tech and we use tech to make our lives easier where possible
-Innovative and inquisitive thinking
+I enhanced projects for company personnel management, significantly reducing the time spent searching for suitable candidates for new positions by utilizing artificial intelligence. Furthermore, we improved the process of requesting leave, transitioning from manual paper-based forms that took hours to sign to an online leave request system that could be signed by all responsible parties within minutes.
+</accomplishments>
 
-What we offer:
-Flexible Remote and/or Hybrid working options
-24 Days annual leave (plus Bank Holidays)
-On-going training where required
-Perks: access to Health Assured Wisdom app
-Competitive salary based on experience
-Modern up to date tech (phones, laptop etc)
+Now, in between the <skills> tags, you'll find skills that I want to highlight on my resume.
+<skills>
+1. React.js & Next.js Proficiency
+2. React-Native Development
+3. Expert in Redux & TypeScript
+4. Mastery of Git & Jira &  CI/CD Pipeline Management
+5. Agile Methodology & Task Estimation
+6. Proficient in Frontend E-commerce Tech.
+</skills>
+Example Role Descriptions
+Below, you'll find examples of well-written role descriptions (hereafter, "Role Description Examples").
 
-On Application:
-Applicants must have a clean UK driving license
-Applicants must be entitled to work in the UK
-Applicants must be willing to be security vetted
-Applicants must have been resident in the UK for 5-years or longer to meet vetting requirements
-Interview Process
-1st Interview - MS Teams 30-60 min call with Head of Software Engineering
-2nd Interview - MS Teams 30-60 min demo and presentation of Tech Task (agreed post 1st Interview) with Head of Software Engineering and Senior Front End Developer
-</jd1>
+• "Led a team on a $40mm SAP implementation for Avenet’s operations in North America and the EMEA region."
+• "Develops R&D partnerships, recruitment programs, and admissions programs for university clients."
+• Managed resources from HQ in Cairo, using a transfer management system to analyze and report on equipment and personnel and facilitate the transfer of resources as requested by unit commanders."
+• "Led the purchasing practice for all telecom-related software components (e.g. VM Ware, Oracle, SAP, and others), designing solutions and long-term maintenance contracts for projects with budgets up to $3.5mm."
+• "Negotiates contracts, develops study protocols and documentation, and manages clinical trials."
+• "Worked on a team of 15 data analysts to tag and enrich data snippets used to train algorithms that identify health and safety hazards for children."
+• "Worked on a team of 15 data analysts to tag and enrich data snippets used to train algorithms that identify health and safety hazards for children."
+• "Led physician practice audits, provided recommendations, and led project billing and invoicing."
+Example Organization Descriptions
+Below, you'll find examples of well-written organization descriptions (hereafter, "Organization Description Examples") in the Eazl resume format.
 
+• "One of Western Europe’s largest providers of IT solutions, machinery, products, logistics, and support to supermarkets, food manufacturers, restaurants, and food wholesalers with ~45 full-time staff."
+• "An IP law firm with ~20 employees providing patent, trademark, design, copyright, litigation, and international intellectual property advice and operational support for enterprise clients like Mitsubishi and a variety of SMEs."
+• "The Austin-headquartered full-service IP firm with ~100 employees serving startups, SMEs, and large firms like Southwest Airlines."
+• "A 5,000-acre vineyard, winery, and tasting room founded in 1994 that produces and sells ~30,000 cases of Bonterra-branded wines annually."
+• "The publicly-traded (NYSE: ABC) drug wholesaling and distributing firm with ~$80bn in annual revenues."
+• "A management consultancy with ~150 employees and offices in Egypt, Dubai, and London that specializes in strategy, change management, and training. Clients include Vodafone, Alcatel, Visa, Barclay’s and others."
+• "The aviation branch of the Egyptian Armed Forces that maintains and operates 1,600+ aircraft and supports more than 50,000 personnel operating through ~500 units across Egypt."
+Resume Bulletpoints Examples
+Below,. you'll find examples of well-written resume bulletpoints (hereafeter, the "Bulletpoint Examples").
+• *Invoicing for Clinical Trials*: Manages disbursement of study payments and fees (for patients, sites, and Institutional Review Boards) and performs regular study budget audits. E.g. successfully managed a remote oncology study for Bristol Myers Squibb (November 2019 - February 2020) with a $300k budget.
+• *Communicating Cross-functionally*: Liaised between Avenet and Deloitte’s technical teams, leading a team of 9 and gathering business requirements for the SAP CRM application and successfully distributing workload to ensure that consultants met 99% utilization requirements (a Deloitte KPI).
+• *Systems Architecture and Integration*: Owns stability of SAP CRM application including system performance, maintenance of the code base, and exception processing. E.g. led the integration of dynamic pricing engine (co-developed with SAP) with ABC’s SAP eCommerce application.
+• *Managing to KPIs*: Leads a team of 20 developers, including contractors from IBM, DCS, and HCL, monitoring personnel budgets and developing governance surrounding risk management, feasibility, demand planning, release planning, and prototyping as is relevant to parts of ABC’s SAP installation.
+• *Strategic IT Project Management*: Engages with strategic IT projects at the manager level. E.g. led the “go live” for SAP Build 4, an 11-month $20mm project, and the accelerated (90-day) integration of 8,000 Walgreens accounts into ABC’s CRM and eCommerce systems (+300% application load).
+• *Communications and Presentations*: Led ~20 in-depth interviews with senior & middle managers to collect strategic information used to identify relevant KPIs and subsequently led multiple training sessions, each with 40+ employees, to educate on the relevance of the metrics and their implementation timeline.
+• *Relationship Management for Complex Projects*: Successfully executed ~25 engagements per year for clients in the telecom, government, and banking sectors. E.g. acted as relationship manager and later won an award from IBM for exceeding KPIs on a $3mm IT project for Egypt Air’s new terminal in Saudi Arabia in 2010. 
+Now, using the Role Information, help me format a description of the organization and my work there. Please use the following format:
 
-Now, if I have any that I want to include, I'm going to list some of my unique talents (hereafter "Unique Talents") in between the <talents> tags.
+# Organization Description
+Here, consider the Information I've included about the Role and any of your training data associated with the Organization and/or Industry and generate a description of the Organization that I can use on my resume. Make this description:
+• less than 200 characters 
+• similar in tone and format to what you see in the Organization Description Examples
+• clear, concise, and action-oriented
 
-<talents>
-</talents>
-Now, I want you to help me develop six skills (hereafter, "the Skills" or individually "the Skill") around which I can build a resume and the text content within my LinkedIn profile. Each Skill you generate should be: 
-1. developed to align with the skills enumerated in My Job Descriptions with priority given to skills listed higher in My Job Descriptions (often, these skills are enumerated in sections called "required qualifications" or "ideal candidate" or similar)
-2. be no longer than 27 characters
-3. if you find any Unique Talents, integrate those into what you generate too
+# Role Description
+Here, consider the Information I've included about the Role--particularly the Description, the Accomplishments, and the number of people on my team and generate a general description of the Role that I can use on my resume. Make this description:
+• less than 200 characters 
+• similar in tone and format to what you see in the Role Description Examples
+• clear, concise, and action-oriented
 
-Collectively, the Skills you generate should represent a candidate that possesses a set of professional skills that aligns with My Job Descriptions.
+# Suggested Bulletpoints
+Here, review the Skills, for each individual skill (hereafter, "the Skill"), review the Accomplishments and:
+1. Generate an alignment score (hereafter, the "Score") between 1 - 5 with 5 representing close alignment between the Skill and information in the Accomplishments and 1 representing that no relevant accomplishments were identified.
+2. Generate a suggested resume bullet point (hereafter, collectively "the Suggested Bulletpoints" and individually the "Suggested Bulletpoint") that highlights information found in the Accomplishments with the Skill. Make this suggested bullet point that's less than 300 characters and is similar in tone and format to what you see in the Bulletpoint Examples, including the bold structure of the Skill followed by the content you generate (like this: *the Skill*: the Suggested Bulletpoint).
 
-Please generate the Skills for me.
-            `,
+Now, sort the Suggested Bulletpoints in order of their respective Score and output them (from those with the highest Scores first to those with the lowest last) with your output structured like this:
+## Suggested Resume Bullet Point for {{the Skill}} (Alignment Score: {{the Score}})
+{{Suggested Bulletpoint}}`,
         },
       ],
       temperature: 1.0,
@@ -140,6 +138,7 @@ Please generate the Skills for me.
     }),
   })
   const responseJSON = await response.json()
+  debugger
   let responseContent = responseJSON.choices[0].message.content
   console.log(responseContent)
   fs.writeFileSync('./work.txt', responseContent)
@@ -151,7 +150,7 @@ const generateImage = async () => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer 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`,
     },
     body: JSON.stringify({
       model: 'dall-e-3',
